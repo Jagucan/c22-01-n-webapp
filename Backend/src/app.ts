9e8bf23d5fcf15284120074e70f6af6ed51395b0
Jacinto Gutierrez <jjgc.2499@gmail.com>
2025-08-17 00:36:53 -0500

    commit

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,24 +1,24 @@
 import app from "./config/app.config";
 import { AppDataSource } from "./config/database.config";
 
-const port = process.env.PORT ?? 4000;
+const port = 4000;
 
 async function startServer() {
-  try {
-    await AppDataSource.initialize()
-      .then(() => {
-        console.log("Database connection established");
-      })
-      .catch((error) => console.error("Database connection error", error));
+	try {
+		await AppDataSource.initialize()
+			.then(() => {
+				console.log("Database connection established");
+			})
+			.catch((error) => console.error("Database connection error", error));
 
-    app.listen(port, () =>
-      console.log(`Server is running at http://localhost:${port}`)
-    );
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error("Error starting the server:", err.message);
-    }
-  }
+		app.listen(port, () =>
+			console.log(`Server is running at http://localhost:${port}`)
+		);
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error("Error starting the server:", err.message);
+		}
+	}
 }
 
 startServer();
